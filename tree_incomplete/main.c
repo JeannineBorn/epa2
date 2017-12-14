e8cf480597a0a1ac736346588089c8e5b8f94d35
JonathanJustavino <s3190208@mail.zih.tu-dresden.de>
2017-12-14 15:15:14 +0100

    Change call by value to call by reference
    Previously the nodes which should be inserted in the tree
    were invoke by their value (struct node) which does not change the tree at all
    Now they are referenced (node_pointer) so the tree is actually modified

--- tree_incomplete/main.c
+++ tree_incomplete/main.c
@@ -1,105 +1,83 @@
+#include <stdbool.h>
 #include "btree/btree.h"
 
-void simple_insert(node n, stdelement element) {
-	int size = n.number_of_elements;
+void simple_insert(node_pointer node, stdelement element) {
+	int size = node->number_of_elements;
 	int buffer_element;
 	for(int i = 0; i < size; i++) {
-		if(n.elements[i] == NULL) {
-			n.elements[i] = element;
+		if(node->elements[i] == NULL) {
+			node->elements[i] = element;
 		}
-		if(element < n.elements[i]) {
-			buffer_element = n.elements[i];
-			n.elements[i] = element;
+		if(element < node->elements[i]) {
+			buffer_element = node->elements[i];
+			node->elements[i] = element;
 			element = buffer_element;
 		}
 	}
-	n.elements[size] = element;
+	node->elements[size] = element;
 }
 
 
-int middle_element(node n, stdelement element) {
+int middle_element(node_pointer node, stdelement element) {
 	int middle;
-	if(element < n.elements[ORDER]) {
-		if(element > n.elements[ORDER - 1]) {
+	if(element < node->elements[ORDER]) {
+		if(element > node->elements[ORDER - 1]) {
 			return element;
 		}
 		else {
-			middle = n.elements[ORDER -1];
-			n.elements[ORDER -1] = NULL;
-			simple_insert(n,element);
+			middle = node->elements[ORDER -1];
+			node->elements[ORDER -1] = NULL;
+			simple_insert(node, element);
 			return middle;
 		}
 	}
-	middle = n.elements[ORDER];
-	n.elements[ORDER] = NULL;
-	simple_insert(n,element);
+	middle = node->elements[ORDER];
+	node->elements[ORDER] = NULL;
+	simple_insert(node, element);
 	return middle;
 }
 
 
-void split(node n, stdelement element, int middle) {
+void split(node_pointer node, stdelement element, int middle) {
 	struct node node_r;
-	node_r.parent = n.parent;
+	node_r.parent = node->parent;
 	for(int i = ORDER; i < MAXNODE; i++){
-		node_r.elements[i - ORDER] = n.elements[i];
-		n.elements[i] = NULL;
+		node_r.elements[i - ORDER] = node->elements[i];
+		node->elements[i] = NULL;
 		node_r.number_of_elements++;
-		n.number_of_elements--;
+		node->number_of_elements--;
 	}
 }
 
 
-void insert_child(node parent, node child) {
-	node_pointer pchild = child;
+void insert_child(node_pointer parent, node_pointer child) {
 	int size = MAXNODE +1;
-	int childcount = parent.number_of_children;
+	int childcount = parent->number_of_children;
 	if(childcount < MAXNODE + 1){
-		parent.children[childcount-1] = pchild;
-		parent.number_of_children++;
+		parent->children[childcount-1] = child;
+		parent->number_of_children++;
 		return;
 	}
 }
 
 
-bool insert_element(stdelement element){
-
-	struct node *current_node = btree->root;
-
-	bool success = !findkey(element, current_node);
-	if(success) {
-		if (!empty(btree)){
-			insert_into_node(current_node, element);
-		}
-		else {
-			struct node node1 = malloc(sizeof(struct node));
-			node1.parent = NULL;
-			node1.number_of_elements = 1;
-			node1.number_of_children = 0;
-			node1.elements[0] = element;
-			btree->root = node1;
-		}
-	}
-	return success;
-}
-
-
-bool findkey(stdelement element, node current_node){
+bool findkey(stdelement element, node_pointer current_node){
 	bool success = false;
-	struct node p = *btree->root;
+	node_pointer p = current_node;
 	//struct node current_node = malloc(sizeof(struct node));
 	while (!success && (p != NULL)){
 		current_node = p;
-		int size_array = (sizeof(current_node.elements)) / (sizeof(int));
+		int size_array = (sizeof(current_node->elements)) / (sizeof(int));
 		for(int i = 0; i < size_array; i++) {
-			if (current_node.elements[i] == element) {
+			if (current_node->elements[i] == element) {
 				success = true;
 			}
-			else if(current_node.elements[i] > element) {
-				p = *(current_node.children[i]);
+			else if(current_node->elements[i] > element) {
+				p = current_node->children[i];
 				continue;
 			}
 		}
-		p = *(current_node.children[MAXNODE]);
+		p = current_node->children[MAXNODE];
 		current_node = p;
 
 	}
@@ -107,32 +85,58 @@ bool findkey(stdelement element, node current_node){
 }
 
 
-void insert_into_node(node n, stdelement element) {
-	int middle = middle_element(n, element);
+bool empty(node_pointer node) {
+	if(node->number_of_elements == 0) {
+		return true;
+	}
+	return false;
+}
+
+
+void insert_into_node(node_pointer node, stdelement element) {
+	int middle = middle_element(node, element);
 
 	// case: the current node is full
-	if(n.number_of_elements > 3) {
+	if(node->number_of_elements > 3) {
 
 		// the parent node is full
-		if(n.parent->children[MAXNODE] != NULL) {
-
+		if(node->parent->children[MAXNODE] != NULL) {
 			// not the simple split
-			insert_into_node(n.parent, middle);
-			split(n, element, middle);
-			insert_child(n.parent, n);
+			insert_into_node(node->parent, middle);
+			split(node, element, middle);
+			insert_child(node->parent, node);
 			return;
 		}
 		else{
-			split(n, element, middle);
-			insert_child(n.parent, n);
+			split(node, element, middle);
+			insert_child(node->parent, node);
 		}
 	}
-	else simple_insert(n,element);
-
+	else simple_insert(node, element);
 }
 
 
+bool insert_element(stdelement element, btree tree){
+
+	node_pointer current_node = tree->root;
+
+	bool success = !findkey(element, current_node);
+	if(success) {
+		if (!empty(tree->root)){
+			insert_into_node(current_node, element);
+		}
+		else {
+			node_pointer node1 = malloc(sizeof(struct node));
+			node1->parent = NULL;
+			node1->number_of_elements = 1;
+			node1->number_of_children = 0;
+			node1->elements[0] = element;
+			tree->root = node1;
+		}
+	}
+	return success;
 }
+
 
 int main(void){
   /*insert your code below*/
